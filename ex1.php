2f0f1deb0274a1224d7f368e9848f2c21dc6f720
gu167 <gumendoncabh@gmail.com>
2022-10-10 12:59:01 -0300

    Calculadora de fatorial

--- ex1.php
+++ ex1.php
@@ -8,8 +8,21 @@
 
 <body>
 <?php
+$n = 10;
+echo "Escreva um número 'n' para calcular seu fatorial<br>";
 
+fatorial($n);
 
+function fatorial($n){
+    $i=$n;
+    $ac=1;
+do{
+    $ac*=$i;
+    $i--;
+}while($i>=1);
+  echo "O fatorial do número é: $ac";
+
+}
 ?>
 </body>
 </html>
