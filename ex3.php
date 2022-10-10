a504db605a4d2ed0f627bfcbd7e6065bf8e49e00
gu167 <gumendoncabh@gmail.com>
2022-10-10 13:29:53 -0300

    Contador de digitos de

---
+++ ex3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+
+<body>
+<?php
+$n=123456789;
+$cons=1;
+    for($c=0;$n>=$cons;$c++){
+
+        $cons=$cons*10;
+    }
+    echo $c;
+
+?>
+</body>
+</html>
