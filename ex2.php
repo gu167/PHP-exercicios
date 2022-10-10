58b53accf10da0a694accda970c1ea55bd97d72c
gu167 <gumendoncabh@gmail.com>
2022-10-10 16:02:15 -0300

    Contador de primos

--- ex2.php
+++ ex2.php
@@ -14,7 +14,7 @@ function primos($n)
 {
     for ($i = 2; $i >= $n; $i++)
         if ($i % 2 == 0 && $i % 3 == 0 && $i % 5 == 0 && $i % 7 == 0 || $i == 2 || $i == 3 || $i == 5 || $i == 7) {
-            echo "<br> $i <br>";
+            echo "<br>$i <br>";
         }
 }
 ?>
