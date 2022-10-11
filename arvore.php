8cec1cbefbe0b77007ffba6afcab6aa5f0fab0c1
gu167 <gumendoncabh@gmail.com>
2022-10-11 19:16:19 -0300

    arvore

---
+++ arvore.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+<?php
+
+$ncam=50;
+echo cresce($ncam)."<br> Feliz Natal e que Jesus elimine todos vocês<br>";
+
+function cresce($ncam){
+
+$estrela=" ☻";
+$tronco="| |";
+$raiz="\=====/";
+$folha="*";
+
+
+echo "<pre>";
+    echo str_pad($estrela, $ncam, " ", STR_PAD_BOTH) . "<br>";
+for ($i=1;$i<=$ncam;$i+=2) {
+    $arvo = str_repeat($folha, $i);
+   $arvo=str_pad($arvo,$ncam," ",STR_PAD_BOTH);
+    echo $arvo."<br>";
+
+}
+if($ncam<=20) {
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
+}
+elseif ($ncam>20 && $ncam<50){
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
+}
+else {
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
+}
+}
+
+?>
+</body>
+</html>
--- ex4.php
+++ ex4.php
@@ -8,7 +8,14 @@
 
 <body>
 <?php
+//&& $y%4!=0
+function dataval($d, $m, $y)
+{
+    $meses=[1=>['dias'=>31, 'nome'=>'janeiro']];
 
+}
+
+dataval(29, 02, 1997);
 
 ?>
 </body>
