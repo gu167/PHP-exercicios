6a058b1f6e0580002ee3ee15432848378c6ddba1
gu167 <gumendoncabh@gmail.com>
2022-10-13 15:58:46 -0300

    Exercicios PHP

---
+++ funcao_assinatura.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+<pre>
+<?php
+
+function signature($nome): string
+{
+    $words = str_word_count($nome, 1);
+    $countWords = count($words);
+    $initial = array();
+
+    for ($i = 0; $i < $countWords; $i++) {
+        if ($countWords == 0) {
+            return "Nome Inválido";
+        }
+        if ($countWords == 1) {
+
+            return strtoupper($nome);
+        } else {
+
+            $initial[] = substr($words[$i-1], 0, 1);
+        }
+    }
+
+    $initialGlue = implode(".", $initial);
+
+    return strtoupper($words[$countWords - 1]) . $initialGlue;
+}
+
+print_r(signature("Gustavo Mendonca Tavares"));
+
+
+?>
+</pre>
+</body>
+</html>
---
+++ sequencia_fibonacci.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+<pre>
+<?php
+
+function fib($n): int
+{
+    $f = array(0, 1,);
+    for ($i = 2; $i <= $n; $i++) {
+        $f[$i] = $f[$i - 1] + $f[$i - 2];
+    }
+
+    return $f[$n];
+}
+fib(10);
+echo ("O número Fibonacci da posição escolhida é: ".fib(10));
+?>
+</pre>
+</body>
+</html>
