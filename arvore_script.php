327b7bcfc27d4af8284ed619912e06a4a43e3d20
PedroSasso1 <pedrosassom@outlook.com>
2022-10-25 11:28:49 -0300

    arvore correction

--- arvore_script.php
+++ arvore_script.php
@@ -1,53 +1,49 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8"/>
     <title></title>
 </head>
 <body>
 <?php
+$ncam = 20;
+echo cresce($ncam) . "<br> Feliz Natal e que Jesus elimine todos vocês<br>";
 
-$ncam=50;
-echo cresce($ncam)."<br> Feliz Natal e que Jesus elimine todos vocês<br>";
+function cresce($ncam)
+{
+    $estrela = " ☻";
+    $tronco = "| |";
+    $raiz = "\=====/";
+    $folha = "*";
 
-function cresce($ncam){
-
-$estrela=" ☻";
-$tronco="| |";
-$raiz="\=====/";
-$folha="*";
-
-
-echo "<pre>";
+    echo "<pre>";
     echo str_pad($estrela, $ncam, " ", STR_PAD_BOTH) . "<br>";
-for ($i=1;$i<=$ncam;$i+=2) {
-    $arvo = str_repeat($folha, $i);
-   $arvo=str_pad($arvo,$ncam," ",STR_PAD_BOTH);
-    echo $arvo."<br>";
 
-}
-if($ncam<=20) {
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
-}
-elseif ($ncam>20 && $ncam<50){
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
-}
-else {
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
-    echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
-}
+    for ($i = 1; $i <= $ncam; $i += 2) {
+        $arvo = str_repeat($folha, $i);
+        $arvo = str_pad($arvo, $ncam, " ", STR_PAD_BOTH);
+        echo $arvo . "<br>";
+    }
+
+    if ($ncam <= 20) {
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    } elseif ($ncam < 50) {
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    } else {
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($tronco, $ncam, " ", STR_PAD_BOTH) . "<br>";
+        echo str_pad($raiz, $ncam, " ", STR_PAD_BOTH) . "<br>";
+    }
 }
 
 ?>
